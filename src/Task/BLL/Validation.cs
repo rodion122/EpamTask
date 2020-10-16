89f83e4691606749079e60bd1415a5452d137f7c
rodion122 <57728693+rodion122@users.noreply.github.com>
2020-10-17 02:51:06 +0300

    made a check on the input data

---
+++ src/Task/BLL/Validation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task.BLL
+{
+    class Validation
+    {
+        public bool IsValidInputData(string[] arrData)
+        {
+            double value;
+            for (int i = 0; i < arrData.Length; i++)
+                if (!double.TryParse(arrData[i], out value))
+                    return false;
+            
+
+            if (arrData.Length < 3)
+                return false;
+
+            if (arrData.Length != 3)
+            {
+                if (arrData.Where(i => Array.IndexOf(arrData, i) % 2 == 0 && i.Count() > 1).Count() > 0)
+                    return false;
+
+                if (arrData.Where(i => Array.IndexOf(arrData, i) % 2 != 0 && i.Count() > 1).Count() > 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
--- src/Task/Program.cs
+++ src/Task/Program.cs
@@ -10,7 +10,6 @@ namespace Task
     {
         static void Main()
         {
-
         }
     }
 }
