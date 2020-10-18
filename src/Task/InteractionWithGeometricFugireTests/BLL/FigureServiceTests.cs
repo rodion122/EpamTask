9296facf5dbca09ef66ac1b2a9bc3d1689eceb6d
rodion122 <57728693+rodion122@users.noreply.github.com>
2020-10-18 23:43:11 +0300

    add not much test

---
+++ src/Task/InteractionWithGeometricFugireTests/BLL/FigureServiceTests.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using InteractionWithGeometricFugire.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InteractionWithGeometricFugire.BLL.Tests
+{
+    [TestClass()]
+    public class FigureServiceTests
+    {
+        [TestMethod()]
+        public void DefineFigureQuadrangle()
+        {
+            // arrange
+            string expects = "Quadrangle";
+            double[] arrCoords = { 1, 3, 2, 1, 7, 2, 5, 4 };
+
+            // act 
+            string value = FigureService.DefineFigure(arrCoords);
+
+            // assert
+            Assert.AreEqual(expects, value);
+        }
+
+        [TestMethod()]
+        public void DefineFigureTriangle()
+        {
+            // arrange
+            string expects = "Triangle";
+            double[] arrCoords = { 1, 3, 3, 3, 3, 1 };
+
+            // act 
+            string value = FigureService.DefineFigure(arrCoords);
+
+            // assert
+            Assert.AreEqual(expects, value);
+        }
+
+        [TestMethod()]
+        public void DefineFigureCirlce()
+        {
+            // arrange
+            string expects = "Circle";
+            double[] arrCoords = { 1, 1, 4 };
+
+            // act 
+            string value = FigureService.DefineFigure(arrCoords);
+
+            // assert
+            Assert.AreEqual(expects, value);
+        }
+
+        [TestMethod()]
+        public void DefineFigureParallelogram()
+        {
+            // arrange
+            string expects = "Parallelogram";
+            double[] arrCoords = { 1, 3, 6, 4, 7, 2, 2, 1 };
+
+            // act 
+            string value = FigureService.DefineFigure(arrCoords);
+
+            // assert
+            Assert.AreEqual(expects, value);
+        }
+
+        [TestMethod()]
+        public void DefineFigurePolygonal()
+        {
+            // arrange
+            string expects = "Polygonal";
+            double[] arrCoords = { 1, 1, 2, 3, 6, 2, 6, 1, 5, 0 };
+
+            // act 
+            string value = FigureService.DefineFigure(arrCoords);
+
+            // assert
+            Assert.AreEqual(expects, value);
+        }
+
+        [TestMethod()]
+        public void DefineFigureTrapeze()
+        {
+            // arrange
+            string expects = "Trapeze";
+            double[] arrCoords = { 1, 5, 3, 9, 11, 1, 5, 1 };
+
+            // act 
+            string value = FigureService.DefineFigure(arrCoords);
+
+            // assert
+            Assert.AreEqual(expects, value);
+        }
+    }
+}
---
+++ src/Task/InteractionWithGeometricFugireTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("InteractionWithGeometricFugireTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InteractionWithGeometricFugireTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("371fb86c-f1ce-4267-bc72-d5ffb3723764")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
